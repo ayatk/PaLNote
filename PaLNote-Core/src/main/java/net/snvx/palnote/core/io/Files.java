8d9f653904945514eecce47898bf62e81950a1a8
snvx <snvx@aol.jp>
2014-12-11 23:07:41 +0900

    modularize the project

---
+++ PaLNote-Core/src/main/java/net/snvx/palnote/core/io/Files.java
@@ -0,0 +1,37 @@
+/*
+ * PaLNote
+ * http://palnote.org
+ *
+ * Copyright (c) 2014 Ayana Tokikaze. All rights reserved.
+ * 　Released under the Apache v2 License.
+ *
+ * Created by Ayana Tokikaze on 14/10/14.
+ */
+package net.snvx.palnote.core.io;
+
+import java.io.File;
+import java.io.IOException;
+
+public class Files {
+    public void NewFile(String dir, String newFileName) {
+        File file = new File(dir + newFileName);
+        try {
+            if (!file.createNewFile()) {
+            }
+        } catch (IOException e) {
+
+        }
+    }
+
+    public void deleteFile() {
+
+    }
+
+    public void createNewDirectory(String newDirectoryName) {
+
+    }
+
+    public void saveFile (String FileName) {
+
+    }
+}
---
+++ PaLNote-Core/src/main/java/net/snvx/palnote/core/view/controller/PreferencesController.java
@@ -0,0 +1,31 @@
+/*
+ * PaLNote
+ * http://palnote.org
+ *
+ * Copyright (c) 2014 Ayana Tokikaze. All rights reserved.
+ * 　Released under the Apache v2 License.
+ *
+ * Created by Ayana Tokikaze on 14/12/06.
+ */
+package net.snvx.palnote.core.view.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class PreferencesController {
+    @FXML
+    private Button general;
+    @FXML
+    private Button edit;
+    @FXML
+    private Button plugin;
+    @FXML
+    private Button language;
+
+    @FXML
+    public void on(ActionEvent event) {
+
+    }
+
+}

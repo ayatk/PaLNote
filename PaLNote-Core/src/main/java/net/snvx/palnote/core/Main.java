61fb38f423264a4430ba8969599f416432452b05
snvx <snvx@aol.jp>
2014-12-13 13:36:27 +0900

    start over

--- PaLNote-Core/src/main/java/net/snvx/palnote/core/Main.java
+++ PaLNote-Core/src/main/java/net/snvx/palnote/core/Main.java
@@ -10,70 +10,14 @@
 package net.snvx.palnote.core;
 
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.scene.Scene;
-import javafx.scene.layout.StackPane;
+import javafx.scene.control.*;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
-import org.fxmisc.richtext.CodeArea;
-import org.fxmisc.richtext.LineNumberFactory;
-import org.fxmisc.richtext.StyleSpans;
-import org.fxmisc.richtext.StyleSpansBuilder;
-
-import java.util.Collection;
-import java.util.Collections;
-import java.util.function.IntFunction;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import net.snvx.palnote.core.ui.AboutAppWindow;
 
 public class Main extends Application {
-
-  private static final String[] KEYWORDS = new String[]{
-          "abstract", "assert", "boolean", "break", "byte",
-          "case", "catch", "char", "class", "const",
-          "continue", "default", "do", "double", "else",
-          "enum", "extends", "final", "finally", "float",
-          "for", "goto", "if", "implements", "import",
-          "instanceof", "int", "interface", "long", "native",
-          "new", "package", "private", "protected", "public",
-          "return", "short", "static", "strictfp", "super",
-          "switch", "synchronized", "this", "throw", "throws",
-          "transient", "try", "void", "volatile", "while"
-  };
-
-  private static final String KEYWORD_PATTERN = "\\b(" + String.join("|", KEYWORDS) + ")\\b";
-  private static final String PAREN_PATTERN = "\\(|\\)";
-  private static final String BRACE_PATTERN = "\\{|\\}";
-  private static final String BRACKET_PATTERN = "\\[|\\]";
-  private static final String SEMICOLON_PATTERN = "\\;";
-  private static final String STRING_PATTERN = "\"([^\"]|\\\")*\"";
-
-  private static final Pattern PATTERN = Pattern.compile(
-          "(?<KEYWORD>" + KEYWORD_PATTERN + ")"
-                  + "|(?<PAREN>" + PAREN_PATTERN + ")"
-                  + "|(?<BRACE>" + BRACE_PATTERN + ")"
-                  + "|(?<BRACKET>" + BRACKET_PATTERN + ")"
-                  + "|(?<SEMICOLON>" + SEMICOLON_PATTERN + ")"
-                  + "|(?<STRING>" + STRING_PATTERN + ")"
-  );
-
-  private static final String sampleCode = String.join("\n", new String[]{
-          "package com.example;",
-          "",
-          "import java.util.*;",
-          "",
-          "public class Foo extends Bar implements Baz {",
-          "",
-          "   public static void main(String[] args) {",
-          "       for(String arg: args) {",
-          "           if(arg.length() != 0)",
-          "               System.out.println(arg);",
-          "           else",
-          "               System.err.println(\"Warning: empty string as argument\");",
-          "       }",
-          "   }",
-          "",
-          "}"
-  });
-
 
   public static void main(String[] args) {
     launch(args);
@@ -81,43 +25,57 @@ public class Main extends Application {
 
   @Override
   public void start(Stage primaryStage) {
-    CodeArea codeArea = new CodeArea();
-    String stylesheet = Main.class.getResource("java.css").toExternalForm();
-    IntFunction<String> format = (digits -> " %" + digits + "d ");
+    primaryStage.setTitle("PaLNote");
+    VBox vBox = new VBox();
+    primaryStage.setScene(new Scene(vBox, 800, 600));
 
-    codeArea.setParagraphGraphicFactory(LineNumberFactory.get(codeArea, format, stylesheet));
-    codeArea.textProperty().addListener((obs, oldText, newText) -> {
-      codeArea.setStyleSpans(0, computeHighlighting(newText));
-    });
-    codeArea.replaceText(0, 0, sampleCode);
+    //メニューバー
+    MenuBar menuBar = new MenuBar();
 
-    Scene scene = new Scene(new StackPane(codeArea), 600, 400);
-    scene.getStylesheets().add(stylesheet);
-    primaryStage.setScene(scene);
-    primaryStage.setTitle("Java Keywords Demo");
+    Menu menuPaLNote = new Menu("PaLNote");
+    MenuItem aboutApp = new MenuItem("About PaLNote");
+    aboutApp.setOnAction((event) -> {
+              Stage aboutStage = new AboutAppWindow();
+              aboutStage.show();
+            }
+    );
+
+    MenuItem preferences = new MenuItem("Preferences...");
+    MenuItem quitApplication = new MenuItem("Quit");
+    quitApplication.setOnAction((event) -> Platform.exit());
+    menuBar.isUseSystemMenuBar();
+    menuPaLNote.getItems().addAll(aboutApp, preferences, quitApplication);
+
+    Menu menuFile = new Menu("File");
+    MenuItem newFile = new MenuItem("New");
+    MenuItem openFile = new MenuItem("Open");
+    MenuItem close = new MenuItem("Close");
+
+    menuFile.getItems().addAll(newFile, openFile, close);
+
+    Menu menuEdit = new Menu("Edit");
+    MenuItem undo = new MenuItem("Undo");
+    MenuItem redo = new MenuItem("Redo");
+    SeparatorMenuItem separatorMenuItem = new SeparatorMenuItem();
+    menuEdit.getItems().addAll(undo, redo, separatorMenuItem);
+
+    Menu menuView = new Menu("View");
+    Menu menuCode = new Menu("Code");
+    menuBar.getMenus().addAll(menuPaLNote, menuFile, menuEdit, menuView, menuCode);
+
+    ToolBar toolBar = new ToolBar();
+
+    Button generalButton = new Button("General");
+    generalButton.setContentDisplay(ContentDisplay.TOP);
+
+    Button pluginButton = new Button("Plugins");
+    pluginButton.setContentDisplay(ContentDisplay.TOP);
+
+    Button editorButton = new Button("Editor");
+    editorButton.setContentDisplay(ContentDisplay.TOP);
+
+    toolBar.getItems().addAll(generalButton, pluginButton, editorButton);
+    vBox.getChildren().addAll(menuBar, toolBar);
     primaryStage.show();
-  }
-
-  private static StyleSpans<Collection<String>> computeHighlighting(String text) {
-    Matcher matcher = PATTERN.matcher(text);
-    int lastKwEnd = 0;
-    StyleSpansBuilder<Collection<String>> spansBuilder
-            = new StyleSpansBuilder<>();
-    while (matcher.find()) {
-      String styleClass =
-              matcher.group("KEYWORD") != null ? "keyword" :
-                      matcher.group("PAREN") != null ? "paren" :
-                              matcher.group("BRACE") != null ? "brace" :
-                                      matcher.group("BRACKET") != null ? "bracket" :
-                                              matcher.group("SEMICOLON") != null ? "semicolon" :
-                                                      matcher.group("STRING") != null ? "string" :
-                                                              null; /* never happens */
-      assert styleClass != null;
-      spansBuilder.add(Collections.emptyList(), matcher.start() - lastKwEnd);
-      spansBuilder.add(Collections.singleton(styleClass), matcher.end() - matcher.start());
-      lastKwEnd = matcher.end();
-    }
-    spansBuilder.add(Collections.emptyList(), text.length() - lastKwEnd);
-    return spansBuilder.create();
   }
 }

1ae6491e87f67dae9f1d9cf494b8a6b0a42105e4
snvx <snvx@aol.jp>
2014-12-13 20:34:21 +0900

    Remove JavaFX controller class

---
+++ PaLNote-Core/src/main/java/core/ui/Keywords.java
@@ -0,0 +1,24 @@
+/*
+ * PaLNote
+ * 
+ * Copyright (c) 2014 Ayana Tokikaze. All rights reserved.
+ * 　Released under the Apache v2 License.
+ *  
+ * Created by Ayana Tokikaze on 14/12/13.
+ */
+package core.ui;
+
+public interface Keywords {
+  static final String[] JAVA_KEYWORDS = new String[] {
+          "abstract", "assert", "boolean", "break", "byte",
+          "case", "catch", "char", "class", "const",
+          "continue", "default", "do", "double", "else",
+          "enum", "extends", "final", "finally", "float",
+          "for", "goto", "if", "implements", "import",
+          "instanceof", "int", "interface", "long", "native",
+          "new", "package", "private", "protected", "public",
+          "return", "short", "static", "strictfp", "super",
+          "switch", "synchronized", "this", "throw", "throws",
+          "transient", "try", "void", "volatile", "while"
+  };
+}
---
+++ PaLNote-Core/src/main/java/core/ui/MainWindow.java
@@ -0,0 +1,74 @@
+/*
+ * PaLNote
+ * http://palnote.org
+ *
+ * Copyright (c) 2014 Ayana Tokikaze. All rights reserved.
+ * 　Released under the Apache v2 License.
+ *
+ * Created by Ayana Tokikaze on 14/10/01.
+ */
+package core.ui;
+
+import javafx.application.Platform;
+import javafx.scene.Scene;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.SeparatorMenuItem;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+
+/**
+ * MainWindowClass
+ */
+public class MainWindow {
+  private Scene Window = null;
+
+  private static int WINDOW_WIDTH = 600;
+  private static int WINDOW_HEIGHT = 500;
+
+  public void mainWindow(Stage stage) {
+    VBox root = new VBox();
+    this.Window = new Scene(root, WINDOW_WIDTH, WINDOW_HEIGHT, Color.WHITE);
+
+    //メニューバー
+    MenuBar menuBar = new MenuBar();
+
+    Menu menuPaLNote = new Menu("PaLNote");
+    MenuItem aboutApp = new MenuItem("About PaLNote");
+    aboutApp.setOnAction((event) -> {
+              Stage aboutStage = new AboutAppWindow();
+              aboutStage.show();
+            }
+    );
+
+    MenuItem preferences = new MenuItem("Preferences...");
+
+    MenuItem quitApplication = new MenuItem("QuitApplication");
+    quitApplication.setOnAction((event) -> Platform.exit());
+
+    menuPaLNote.getItems().addAll(aboutApp, preferences, quitApplication);
+
+    Menu menuFile = new Menu("File");
+    MenuItem newFile = new MenuItem("New");
+    MenuItem openFile = new MenuItem("Open");
+    MenuItem close = new MenuItem("Close");
+
+    menuFile.getItems().addAll(newFile, openFile, close);
+
+    Menu menuEdit = new Menu("Edit");
+    MenuItem undo = new MenuItem("Undo");
+    MenuItem redo = new MenuItem("Redo");
+    SeparatorMenuItem separatorMenuItem = new SeparatorMenuItem();
+    menuEdit.getItems().addAll(undo, redo, separatorMenuItem);
+
+    Menu menuView = new Menu("View");
+    Menu menuCode = new Menu("Code");
+    menuBar.getMenus().addAll(menuPaLNote, menuFile, menuEdit, menuView, menuCode);
+
+
+    root.getChildren().addAll(menuBar);
+
+  }
+}

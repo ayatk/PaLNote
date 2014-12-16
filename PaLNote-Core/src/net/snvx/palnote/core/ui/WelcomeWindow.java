e71e57a12cdcb8265e108998726852699f51e7ac
snvx <snvx@aol.jp>
2014-12-16 16:43:35 +0900

    Add WelcomeWindow class file

---
+++ PaLNote-Core/src/net/snvx/palnote/core/ui/WelcomeWindow.java
@@ -0,0 +1,18 @@
+/*
+ * PaLNote
+ * http://palnote.org
+ *
+ * Copyright (c) 2014 Ayana Tokikaze. All rights reserved.
+ * 　Released under the Apache v2 License.
+ *
+ * Created by Ayana Tokikaze on 14/10/07.
+ */
+package net.snvx.palnote.core.ui;
+
+import javafx.stage.Stage;
+
+public class WelcomeWindow extends Stage {
+  public WelcomeWindow() {
+
+  }
+}

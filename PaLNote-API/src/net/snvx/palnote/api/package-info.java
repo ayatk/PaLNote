f5b8aad7082f9407010b9aff8a00a025258f2e4f
snvx <snvx@aol.jp>
2014-12-18 00:35:13 +0900

    Added package info

---
+++ PaLNote-API/src/net/snvx/palnote/api/package-info.java
@@ -0,0 +1,13 @@
+/*
+ * PaLNote
+ * 
+ * Copyright (c) 2014 Ayana Tokikaze. All rights reserved.
+ * 　Released under the Apache v2 License.
+ *  
+ * Created by Ayana Tokikaze on 14/12/17.
+ */
+
+/**
+ * プラグインに提供するAPI群のモジュールです
+ */
+package net.snvx.palnote.api;
---
+++ PaLNote-Core/src/net/snvx/palnote/core/plugin/pluginInfo.java
@@ -0,0 +1,12 @@
+/*
+ * PaLNote
+ * 
+ * Copyright (c) 2014 Ayana Tokikaze. All rights reserved.
+ * 　Released under the Apache v2 License.
+ *  
+ * Created by Ayana Tokikaze on 14/12/17.
+ */
+package net.snvx.palnote.core.plugin;
+
+public interface pluginInfo {
+}

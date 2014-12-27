ef0059098ebb1f483a3a6df69b06af104480707c
snvx <snvx@aol.jp>
2014-12-27 21:31:26 +0900

    Refactoring￼:v:

---
+++ PaLNote-Core/src/net/snvx/palnote/core/Main.java
@@ -0,0 +1,25 @@
+/*
+ * PaLNote
+ * http://palnote.org
+ *
+ * Copyright (c) 2014 Ayana Tokikaze. All rights reserved.
+ * 　Released under the Apache v2 License.
+ *
+ * Created by Ayana Tokikaze on 14/09/24.
+ */
+package net.snvx.palnote.core;
+
+import net.snvx.palnote.core.ui.MainWindow;
+
+/**
+ * Main Class
+ */
+public class Main {
+  /**
+   * main method
+   * @param args
+   */
+  public static void main(String[] args) {
+    MainWindow.appLaunch(args);
+  }
+}

e8d304f0d075335035e142bc366d336df642f1b4
snvx <snvx@aol.jp>
2014-12-27 21:30:52 +0900

    Refactoring:point_up:

--- PaLNote-Core/src/net/snvx/palnote/core/ui/MainWindow.java
+++ PaLNote-Core/src/net/snvx/palnote/core/ui/MainWindow.java
@@ -9,30 +9,47 @@
  */
 package net.snvx.palnote.core.ui;
 
+import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.scene.Scene;
-import javafx.scene.control.Menu;
-import javafx.scene.control.MenuBar;
-import javafx.scene.control.MenuItem;
-import javafx.scene.control.SeparatorMenuItem;
+import javafx.scene.control.*;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
-import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import net.snvx.palnote.core.ui.preferences.PreferencesWindow;
+import org.fxmisc.richtext.CodeArea;
+import org.fxmisc.richtext.LineNumberFactory;
 
 /**
  * MainWindowClass
  */
-public class MainWindow {
-  private Scene Window = null;
+public class MainWindow extends Application {
 
-  private static int WINDOW_WIDTH = 600;
-  private static int WINDOW_HEIGHT = 500;
+  protected static int WINDOW_WIDTH = 1200;
+  protected static int WINDOW_HEIGHT = 800;
+  protected static final double BUTTON_WIDTH = 40;
+  protected static final double BUTTON_HEIGHT = 35;
 
-  public void mainWindow(Stage stage) {
-    VBox root = new VBox();
-    this.Window = new Scene(root, WINDOW_WIDTH, WINDOW_HEIGHT, Color.WHITE);
+  protected static final String ICON_PATH = "net/snvx/palnote/resources/images/basic/";
 
-    //メニューバー
+  public static void appLaunch(String[] args) {
+    launch();
+  }
+
+  //TODO もっとコードを短くしたい
+  /**
+   * Application run
+   * @see javafx.application.Application#start(javafx.stage.Stage)
+   */
+  @Override
+  public void start(Stage primaryStage) {
+    primaryStage.setTitle("PaLNote");
+    VBox vBox = new VBox();
+    primaryStage.setScene(new Scene(vBox, WINDOW_WIDTH, WINDOW_HEIGHT));
+
+    //TODO メニューバーもメソッド化しちゃおうね〜
     MenuBar menuBar = new MenuBar();
 
     Menu menuPaLNote = new Menu("PaLNote");
@@ -44,10 +61,14 @@ public class MainWindow {
     );
 
     MenuItem preferences = new MenuItem("Preferences...");
-
-    MenuItem quitApplication = new MenuItem("QuitApplication");
+    preferences.setOnAction((event) -> {
+              Stage preferencesStage = new PreferencesWindow();
+              preferencesStage.show();
+            }
+    );
+    MenuItem quitApplication = new MenuItem("Quit");
     quitApplication.setOnAction((event) -> Platform.exit());
-
+    menuBar.setUseSystemMenuBar(true);
     menuPaLNote.getItems().addAll(aboutApp, preferences, quitApplication);
 
     Menu menuFile = new Menu("File");
@@ -67,8 +88,71 @@ public class MainWindow {
     Menu menuCode = new Menu("Code");
     menuBar.getMenus().addAll(menuPaLNote, menuFile, menuEdit, menuView, menuCode);
 
+    ToolBar toolBar = new ToolBar();
 
-    root.getChildren().addAll(menuBar);
+    //TODO ボタンの処理をメソッド化する
+    Button saveButton = new Button();
+    saveButton.setGraphic(new ImageView(ICON_PATH + "ic_save_white_24dp.png"));
+    saveButton.setPrefSize(BUTTON_WIDTH, BUTTON_HEIGHT);
 
+    Button undoButton = new Button();
+    undoButton.setGraphic(new ImageView(ICON_PATH + "ic_undo_white_24dp.png"));
+    undoButton.setPrefSize(BUTTON_WIDTH, BUTTON_HEIGHT);
+
+    Button redoButton = new Button();
+    redoButton.setGraphic(new ImageView(ICON_PATH + "ic_redo_white_24dp.png"));
+    redoButton.setPrefSize(BUTTON_WIDTH, BUTTON_HEIGHT);
+
+    Button copyButton = new Button();
+    copyButton.setGraphic(new ImageView(ICON_PATH + "ic_content_copy_white_24dp.png"));
+    copyButton.setPrefSize(BUTTON_WIDTH, BUTTON_HEIGHT);
+
+    Button consoleButton = new Button();
+    consoleButton.setGraphic(new ImageView(ICON_PATH + "console_white.png"));
+    consoleButton.setPrefSize(BUTTON_WIDTH, BUTTON_HEIGHT);
+
+    Button settingButton = new Button();
+    settingButton.setGraphic(new ImageView(ICON_PATH + "ic_settings_white_24dp.png"));
+    settingButton.setPrefSize(BUTTON_WIDTH, BUTTON_HEIGHT);
+    settingButton.setOnAction((event) -> {
+              Stage preferencesStage = new PreferencesWindow();
+              preferencesStage.show();
+            }
+    );
+
+    Button bugReportButton = new Button();
+    bugReportButton.setGraphic(new ImageView(ICON_PATH + "ic_bug_report_white_24dp.png"));
+    bugReportButton.setPrefSize(BUTTON_WIDTH, BUTTON_HEIGHT);
+
+    Button helpButton = new Button();
+    helpButton.setGraphic(new ImageView(ICON_PATH + "ic_help_white_24dp.png"));
+    helpButton.setPrefSize(BUTTON_WIDTH, BUTTON_HEIGHT);
+
+    Button searchButton = new Button();
+    searchButton.setGraphic(new ImageView(ICON_PATH + "ic_search_white_24dp.png"));
+    searchButton.setPrefSize(BUTTON_WIDTH, BUTTON_HEIGHT);
+
+    toolBar.getItems().addAll(
+            saveButton,
+            undoButton,
+            redoButton,
+            copyButton,
+            consoleButton,
+            settingButton,
+            bugReportButton,
+            helpButton,
+            searchButton
+    );
+
+    HBox hBox = new HBox();
+    VBox.setVgrow(hBox, Priority.ALWAYS);
+    //TODO シンタックスハイライトの実装
+    CodeArea codeArea = new CodeArea();
+    codeArea.setParagraphGraphicFactory(LineNumberFactory.get(codeArea));
+    HBox.setHgrow(codeArea, Priority.ALWAYS);
+
+    hBox.getChildren().addAll(codeArea);
+    vBox.getChildren().addAll(menuBar, toolBar, hBox);
+    primaryStage.show();
   }
 }
